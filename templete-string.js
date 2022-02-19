4c368cf2c4947135fc0187298858d288811289d2
Shidul Islam <sishahin093@gmail.com>
2022-02-19 13:37:55 +0600

    31-5 Template string, multiple line string, dynamic string

---
+++ templete-string.js
@@ -0,0 +1,20 @@
+// const demo = "My name is shainn \n My Mother name is Pervin Akter \n my Father name is Jahangir alom";
+// console.log(demo);
+
+// const demo = `My name is shainn 
+//     My Mother name is Pervin Akter 
+//      my Father name is Jahangir alom`;
+// console.log(demo);
+
+// const count = 5;
+// const old = `My friend serial is : ${count}`;
+// console.log(old);
+
+// const friends = ['abul', 'babul', 'sabulla'];
+// const friend = `My friend serial is : ${friends.length}`;
+// console.log(friend);
+
+const frist = 'Shidul';
+const last = 'Islam';
+const fullName = `${frist} ${last}`;
+console.log(fullName);
