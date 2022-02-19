5a5c44215af7039a976dc690f5ad60eec99052e1
Shidul Islam <sishahin093@gmail.com>
2022-02-19 12:37:43 +0600

    31-3 Breakup with var to have a relationship with Let and const

---
+++ let-const.js
@@ -0,0 +1,22 @@
+// const balance = 1240;
+balance = 1340; // not allowed reassign const variable. 
+
+const userName = 'janpahi potas potas ';
+const weTogether = 'ami ' + userName;
+console.log(weTogether);
+
+const numbers = [34, 54, 23, 56, 23, 6, 42, 64];
+// numbers = [45, 73, 73];  not allowed re-assign const variable in array.
+// numbers[2] = 000111; // alow
+// console.log(numbers);
+
+// const stuent = { roll: 101, name: 'shain', job: 'interb' };
+// console.log(stuent);
+// stuent.name = "shidul Islam";
+// console.log(stuent);
+
+for (let i = 0; i <= numbers.length; i++) {
+    const number = numbers[i];
+    console.log(number);
+}
+
