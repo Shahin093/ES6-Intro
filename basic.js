2ab0e66f9450db5111fdeb08c9cdb73bfb88f880
Shidul Islam <sishahin093@gmail.com>
2022-02-19 08:50:58 +0600

    31-1 Simple Recap of JavaScript six core parts

---
+++ basic.js
@@ -0,0 +1,58 @@
+// variable declear 
+var deposit = 400;
+// Condition 
+if (deposit > 500) {
+
+}
+if (deposit < 200) {
+
+}
+if (deposit == 500) {
+
+}
+if (deposit != 500) {
+
+}
+if (deposit >= 500) {
+
+}
+if (deposit <= 500) {
+
+}
+else {
+
+}
+// array 
+const numbers = [45, 23, 67, 23, 854, 234, 563];
+// array length 
+const numberCount = numbers.length;
+// array push 
+numbers.push(1212121);
+//  array pop
+numbers.pop();
+// array index value change 
+numbers[2] = 5656565;
+// check whether 222 included in the array 
+if (numbers.indexOf(222) != -1) {
+
+}
+// included function 
+if (numbers.includes(222)) {
+
+}
+
+// Loop 
+// while , for 
+for (const number of numbers) {
+
+}
+
+// function 
+function fullName(first, second) {
+    const name = first + ' ' + second;
+
+}
+const person = fullName('Rahin', 'Ali');
+
+// object 
+const bottle = { color: 'Yellow', contain: 'water', price: 550 };
